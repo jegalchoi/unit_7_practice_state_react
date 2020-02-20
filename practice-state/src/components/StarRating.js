46408fe08dfd35338a0cc56fedc6efc2c129a2fe
jegalchoi <thefreshbegal@gmail.com>
2020-02-19 18:02:09 -0800

    Complete practice

--- practice-state/src/components/StarRating.js
+++ practice-state/src/components/StarRating.js
@@ -4,19 +4,32 @@ import Star from './Star';
 class StarRating extends Component {
 
   // Initialize a 'rating' state
-
+  state = {
+    rating: 0
+  }
 
   // Write a function that returns 5 Star components
+renderStars = () => {
+  let stars = []
+  let maxRating = 5
 
-
+  for(let i = 1; i <= maxRating; i += 1) {
+    stars.push(<Star id={i} key={i} onClick={this.handleClick} rating={this.state.rating} />)
+  }
+  return stars
+}
   // Write an event handler that updates the rating state.
   // Pass the function to a Star component via props
-
-
+  handleClick = (id) => {
+    let rating = (id === this.state.rating) ? 0 : id
+    this.setState({
+      rating
+    })
+  }
   render() {
     return (
       <ul className="course--stars">
-        {/* Render the Star components */}
+        {this.renderStars()}
       </ul>
     );
   }
